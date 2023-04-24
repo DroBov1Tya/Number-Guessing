22aebda26efeda1df9837e447c2f3a2ba2f76301
DroBoV1tya <95350284+DroBov1Tya@users.noreply.github.com>
2023-04-24 20:28:48 +0300

    version 0.1

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
 import random
+rand1 = int(input('From:'))
+rand2 = int(input('To:'))
+
+
+print("Hi! Let's play a game.\nI choose a number from", rand1, 'to', rand2, "you guess it. ")
 score = 0
-a = int(input())
-def random_number(x):
-    x = random.randint(0, 10)
-    return x
+i = 0
+
+while i < 10:    
+    x = random.randint(rand1,rand2)
+
+    Num1 = int(input('print your number: '))
+    Num2 = x
+
+    if Num1 == Num2:
+        score = score + 1
+        i = i + 1
+        print('Right! \nmaybe you wanna play again?')
+        
+    else:
+        print('Wrong answer... \nGame over\n','(Rigt answer be:',x,')')
+
+    print(score)
+
+    if score == 10:
+        print('You win')
+    else:
+        None
